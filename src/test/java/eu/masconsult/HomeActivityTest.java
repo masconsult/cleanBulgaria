69b26a07842dbf006f0ea82163cd987b79c4d181
Venelin Valkov <venelin@masconsult.eu>
2011-09-09 12:11:00 +0300

    Added default maven structure and hamcres matchers in the pom

--- src/test/java/eu/masconsult/HomeActivityTest.java
+++ src/test/java/eu/masconsult/HomeActivityTest.java
@@ -4,17 +4,17 @@ import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * User: Venelin Valkov <venelin@masconsult.eu>
- * Date: 08-09-2011
+ * Date: 09-09-2011
  */
 @RunWith(RobolectricTestRunner.class)
-public class HomeTest {
+public class HomeActivityTest {
 
   @Test
-  public void shouldExistActivity() {
+  public void shouldHaveActivity() {
     assertNotNull(new HomeActivity());
   }
 }
