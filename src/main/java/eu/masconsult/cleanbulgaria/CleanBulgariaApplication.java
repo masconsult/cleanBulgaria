69da2f67d46193453a32f5c2eacfae2707b82fb2
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-09-12 12:53:26 +0300

    Change app name and created package cleanbulgaria

---
+++ src/main/java/eu/masconsult/cleanbulgaria/CleanBulgariaApplication.java
@@ -0,0 +1,7 @@
+package eu.masconsult.cleanbulgaria;
+
+import roboguice.application.RoboApplication;
+
+public class CleanBulgariaApplication extends RoboApplication {
+
+}
---
+++ src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
@@ -0,0 +1,11 @@
+package eu.masconsult.cleanbulgaria;
+
+import android.os.Bundle;
+import roboguice.activity.RoboActivity;
+
+public class LoginActivity extends RoboActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
