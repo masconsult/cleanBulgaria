80698f50c0e6afb66736d890268ffd30c3b34841
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-09-12 18:25:03 +0300

    Created Simple LoginActivity

--- src/main/java/eu/masconsult/cleanbulgaria/CleanBulgariaApplication.java
+++ src/main/java/eu/masconsult/cleanbulgaria/CleanBulgariaApplication.java
@@ -1,7 +1,14 @@
 package eu.masconsult.cleanbulgaria;
 
+import java.util.List;
+
+import com.google.inject.Module;
+
 import roboguice.application.RoboApplication;
 
 public class CleanBulgariaApplication extends RoboApplication {
 
+	 protected void addApplicationModules(List<Module> modules) {
+	        
+	 }
 }
--- src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
+++ src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
@@ -1,11 +1,49 @@
 package eu.masconsult.cleanbulgaria;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 
 public class LoginActivity extends RoboActivity {
+	
+	@InjectView(R.id.emailText)
+	EditText emailTextEdit;
+	
+	@InjectView(R.id.passwordText)
+	EditText passwordTextEdit;
+	
+	@InjectView(R.id.loginButton)
+	Button loginButton;
+	
+	Toast validationToast;
+	
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_layout);
+		validationToast = Toast.makeText(getApplicationContext(), "", 2);
+		
+		loginButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(emailTextEdit.getText().toString().isEmpty() || passwordTextEdit.getText().toString().isEmpty()) {
+					validationToast.setText("Попълнете Полетата");
+					validationToast.show();
+					return;
+				}
+				if(!emailTextEdit.getText().toString().contains("@")) {
+					validationToast.setText("Невалиден Е-мейл");
+					validationToast.show();
+				}
+			}
+		});
 	}
 }
