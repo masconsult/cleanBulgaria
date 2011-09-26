af69ec1c2a5686c58a3eece406f26c2a8c9cf57b
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-09-26 17:52:00 +0300

    Wrap exceptions thrown by the client

--- src/main/java/eu/masconsult/cleanbulgaria/connection/Connection.java
+++ src/main/java/eu/masconsult/cleanbulgaria/connection/Connection.java
@@ -1,11 +1,8 @@
 package eu.masconsult.cleanbulgaria.connection;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,32 +10,24 @@ import java.util.List;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.protocol.ClientContext; 
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
 
 public class Connection {
 
-	
+
+
 	private HttpClient client;
 	private HttpContext context;
 	private int statusCode;
@@ -53,45 +42,35 @@ public class Connection {
 	}
 
 	public void login(final String email, final String password) throws InvalidDataException, ConnectionException {
-		
+
 		HttpResponse loginResponse;
-		String pageSource = null;
 		try {
 			loginResponse = doLoginPost(email, password);
-		} catch (Exception e) {
+		} catch (IOException e) {
 			throw new ConnectionException(e);
 		}
-		
-		try {
-			pageSource = EntityUtils.toString(loginResponse.getEntity());
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 		Header redirectHeader = loginResponse.getFirstHeader("Location");
-		
+
 		if(redirectHeader == null) {
 			throw new InvalidDataException();
 		}
-		
+
 		if(!redirectHeader.getValue().equals("index.php")) {
 			throw new InvalidDataException();
 		}
-		
-		
+
+
 		int connectionStatus = loginResponse.getStatusLine().getStatusCode();
 		statusCode = connectionStatus;
 		loggedIn = true;
 	}
-	
+
 	public int getConnectionStatusCode() {
 		return statusCode;
 	}
 
-	private HttpResponse doLoginPost(final String email, final String password) throws IOException {
+	private HttpResponse doLoginPost(final String email, final String password) throws IOException, ClientProtocolException {
 		List<NameValuePair> loginParameters = setUpLoginParameters(email, password);
 		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(loginParameters, "UTF-8");
 		HttpPost post = new HttpPost("http://ng.btv.bg/map/login.php");
@@ -108,37 +87,37 @@ public class Connection {
 		formParameters.add(new BasicNameValuePair("submit", "Вход"));
 		return formParameters;
 	}
-	
+
 	public void mark(final MarkRequestData data) throws InvalidDataException, IOException {
 		if(!loggedIn) {
 			throw new ClientProtocolException();
 		}
 		HttpResponse response = doMarkPost(data);
-		
+
 		Header redirectHeader = response.getFirstHeader("Location");
-		
+
 		if(redirectHeader == null) {
 			throw new InvalidDataException();
 		}
-		
+
 		if(!redirectHeader.getValue().equals("thankyou.php")) {
 			throw new InvalidDataException();
 		}
 		int connectionStatus = response.getStatusLine().getStatusCode();
-		
+
 		this.statusCode = connectionStatus;
 	}
-	
+
 	private HttpResponse doMarkPost(final MarkRequestData data) throws ClientProtocolException, IOException {
 		MultipartEntity entity = setUpMarkParameters(data);
 		HttpPost markPost = new HttpPost("http://ng.btv.bg/map/add_waste_info.php");
 		markPost.setEntity(entity);
 		return client.execute(markPost);
 	}
-	
+
 	private MultipartEntity setUpMarkParameters(final MarkRequestData data) throws UnsupportedEncodingException {
 		MultipartEntity entity = new MultipartEntity( HttpMultipartMode.BROWSER_COMPATIBLE );
-		
+
 		entity.addPart( "file[]", new FileBody(data.imageFile, "image/jpg"));
 		entity.addPart("address", new StringBody(data.address, "text/plain", utfSet));
 		entity.addPart("lat", new StringBody(data.lat, utfSet));
@@ -152,10 +131,10 @@ public class Connection {
 		entity.addPart("submit.x", new StringBody(data.submitX, utfSet));
 		entity.addPart("submit.y", new StringBody(data.submitY, utfSet));
 		entity.addPart("go", new StringBody("go", utfSet));
-	
+
 		return entity;
 	}
-	
+
 	public boolean isLoggedIn() {
 		return loggedIn;
 	}
