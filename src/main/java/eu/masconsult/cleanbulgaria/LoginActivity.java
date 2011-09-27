c20aafab66ab267b1ee3890b20d1ff5c81270e52
Venelin Valkov <v.valkov@ymail.com>
2011-09-27 10:32:42 +0300

    Loggin process now uses async connection call

--- src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
+++ src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
@@ -1,16 +1,10 @@
 package eu.masconsult.cleanbulgaria;
 
 
-
-import java.io.IOException;
-
-import com.google.inject.Inject;
-
-import eu.masconsult.cleanbulgaria.connection.Connection;
-import eu.masconsult.cleanbulgaria.connection.ConnectionException;
-import eu.masconsult.cleanbulgaria.connection.InvalidDataException;
-
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,69 +14,111 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.inject.Inject;
+import eu.masconsult.cleanbulgaria.connection.Connection;
+import eu.masconsult.cleanbulgaria.connection.ConnectionException;
+import eu.masconsult.cleanbulgaria.connection.InvalidDataException;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 
 public class LoginActivity extends RoboActivity {
-	
-	@InjectView(R.id.emailText)
-	EditText emailTextEdit;
-	
-	@InjectView(R.id.passwordText)
-	EditText passwordTextEdit;
-	
-	@InjectView(R.id.loginButton)
-	Button loginButton;
-	
-	@Inject
-	Connection connection;
-	
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_layout);
-		emailTextEdit.setText("dani7@abv.bg");
-		passwordTextEdit.setText("alabala");
-		
-		
-		passwordTextEdit.setOnKeyListener(new OnKeyListener() {
-			
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if(event.getAction() == EditorInfo.IME_ACTION_DONE || event.getAction() == EditorInfo.IME_NULL) {
-					loginButton.performClick();
-					return true;
-				}
-				return false;
-			}
-		});
-		
-		loginButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-	
-				String email = emailTextEdit.getText().toString();
-				String password  = passwordTextEdit.getText().toString();
-				if(email.equals("") || password.equals("")) {
-					Toast validationToast = Toast.makeText(getApplicationContext(), "Попълнете Полетата", 2);
-					validationToast.show();
-					return;
-				}
-				try {		
-					connection.login(email, password);
-					startActivity(new Intent(getApplicationContext(), MainActivity.class));
-				} catch (InvalidDataException e) {
-					Toast validationToast = Toast.makeText(getApplicationContext(), "Невалиден Е-Мейл или парола", 2);
-					validationToast.show();
-					return;
-				} catch (ConnectionException e) {
-					Toast.makeText(getBaseContext(), "Възникна проблем при свързване", 3).show();
-					return;
-				}
-				
-			}
-		});
-	}
+
+    @InjectView(R.id.emailText)
+    EditText emailTextEdit;
+
+    @InjectView(R.id.passwordText)
+    EditText passwordTextEdit;
+
+    @InjectView(R.id.loginButton)
+    Button loginButton;
+
+    @Inject
+    Connection connection;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        emailTextEdit.setText("dani7@abv.bg");
+        passwordTextEdit.setText("alabala");
+
+
+        passwordTextEdit.setOnKeyListener(new OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == EditorInfo.IME_ACTION_DONE || event.getAction() == EditorInfo.IME_NULL) {
+                    loginButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String email = emailTextEdit.getText().toString();
+                String password = passwordTextEdit.getText().toString();
+                if (email.equals("") || password.equals("")) {
+                    Toast validationToast = Toast.makeText(getApplicationContext(), "Попълнете Полетата", 2);
+                    validationToast.show();
+                    return;
+                }
+                Context context = getApplicationContext();
+                ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, context.getString(R.string.loginProcessTitle), context.getString(R.string.loginProcessMessage));
+                new LoginTask(getApplicationContext(), progressDialog).execute(email, password);
+            }
+        });
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, Integer> {
+
+        private Context context;
+        private ProgressDialog progressDialog;
+
+        private static final int LOGIN_SUCCESS = 1;
+        private static final int INVALID_CREDENTIALS = 2;
+        private static final int NO_CONNECTION = 3;
+
+        public LoginTask(Context context, ProgressDialog progressDialog) {
+            this.context = context;
+            this.progressDialog = progressDialog;
+        }
+
+        @Override
+        protected Integer doInBackground(String... userCredentials) {
+            try {
+                connection.login(userCredentials[0], userCredentials[1]);
+            } catch (InvalidDataException e) {
+                return INVALID_CREDENTIALS;
+            } catch (ConnectionException e) {
+                return NO_CONNECTION;
+            }
+            return LOGIN_SUCCESS;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            switch (result) {
+                case LOGIN_SUCCESS:
+                    Intent intent = new Intent(context, MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+                case INVALID_CREDENTIALS:
+                    Toast.makeText(context, context.getString(R.string.invalidCredentials), Toast.LENGTH_LONG).show();
+                    break;
+
+                case NO_CONNECTION:
+                    Toast.makeText(context, context.getString(R.string.noConnection), Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
 }
