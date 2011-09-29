b16cd60025ca024f96adfbd0d998344491e5e080
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-09-29 16:00:07 +0300

    Fixed back button on login.

--- src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
+++ src/main/java/eu/masconsult/cleanbulgaria/LoginActivity.java
@@ -1,12 +1,11 @@
 package eu.masconsult.cleanbulgaria;
 
 
-import android.app.AlertDialog;
-import android.app.Dialog;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -16,125 +15,131 @@ import android.view.View.OnKeyListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import com.google.inject.Inject;
+
 import eu.masconsult.cleanbulgaria.connection.Connection;
 import eu.masconsult.cleanbulgaria.connection.ConnectionException;
 import eu.masconsult.cleanbulgaria.connection.InvalidDataException;
-import roboguice.activity.RoboActivity;
-import roboguice.event.Observes;
-import roboguice.inject.InjectView;
-import roboguice.util.RoboAsyncTask;
 
 public class LoginActivity extends RoboActivity {
 
-    @InjectView(R.id.emailText)
-    private EditText emailTextEdit;
+	@InjectView(R.id.emailText)
+	private EditText emailTextEdit;
 
-    @InjectView(R.id.passwordText)
-    private EditText passwordTextEdit;
+	@InjectView(R.id.passwordText)
+	private EditText passwordTextEdit;
 
-    @InjectView(R.id.loginButton)
-    private Button loginButton;
+	@InjectView(R.id.loginButton)
+	private Button loginButton;
 
-    @Inject
-    private Connection connection;
+	@Inject
+	private Connection connection;
 
-    private LoginTask loginTask;
+	private LoginTask loginTask;
 
-    private ProgressDialog progressDialog;
+	private ProgressDialog progressDialog;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        loginTask = new LoginTask(getApplicationContext());
-        setContentView(R.layout.login_layout);
-        emailTextEdit.setText("dani7@abv.bg");
-        passwordTextEdit.setText("alabala");
+		super.onCreate(savedInstanceState);
+		loginTask = new LoginTask(getApplicationContext());
+		setContentView(R.layout.login_layout);
+		emailTextEdit.setText("dani7@abv.bg");
+		passwordTextEdit.setText("alabala");
 
-        progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
-        progressDialog.setTitle(R.string.loginProcessTitle);
-        progressDialog.setMessage(getString(R.string.loginProcessMessage));
+		progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+		progressDialog.setTitle(R.string.loginProcessTitle);
+		progressDialog.setMessage(getString(R.string.loginProcessMessage));
 
-        passwordTextEdit.setOnKeyListener(new OnKeyListener() {
+		passwordTextEdit.setOnKeyListener(new OnKeyListener() {
 
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == EditorInfo.IME_ACTION_DONE || event.getAction() == EditorInfo.IME_NULL) {
-                    loginButton.performClick();
-                    return true;
-                }
-                return false;
-            }
-        });
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == EditorInfo.IME_ACTION_DONE || event.getAction() == EditorInfo.IME_NULL) {
+					loginButton.performClick();
+					return true;
+				}
+				return false;
+			}
+		});
 
-        loginButton.setOnClickListener(new OnClickListener() {
+		loginButton.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
+			@Override
+			public void onClick(View v) {
 
-                String email = emailTextEdit.getText().toString();
-                String password = passwordTextEdit.getText().toString();
-                if (email.equals("") || password.equals("")) {
-                    Toast validationToast = Toast.makeText(getApplicationContext(), "Попълнете Полетата", 2);
-                    validationToast.show();
-                    return;
-                }
-                loginTask.execute(email, password);
-            }
-        });
-    }
-    
-    private class LoginTask extends AsyncTask<String, Void, Integer> {
+				String email = emailTextEdit.getText().toString();
+				String password = passwordTextEdit.getText().toString();
+				if (email.equals("") || password.equals("")) {
+					Toast validationToast = Toast.makeText(getApplicationContext(), "Попълнете Полетата", 2);
+					validationToast.show();
+					return;
+				}
+				loginTask.execute(email, password);
+			}
+		});
+	}
 
-        private Context context;
+	@Override
+	public void onBackPressed() {
+		Intent setIntent = new Intent(Intent.ACTION_MAIN);
+		setIntent.addCategory(Intent.CATEGORY_HOME);
+		setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(setIntent); 
+		return;
+	}
 
-        private static final int LOGIN_SUCCESS = 1;
-        private static final int INVALID_CREDENTIALS = 2;
-        private static final int NO_CONNECTION = 3;
+	private class LoginTask extends AsyncTask<String, Void, Integer> {
 
-        @Inject
-        public LoginTask(Context context) {
-            this.context = context;
-        }
+		private Context context;
 
-        @Override
-        protected void onPreExecute() {
-            progressDialog.show();
-        }
+		private static final int LOGIN_SUCCESS = 1;
+		private static final int INVALID_CREDENTIALS = 2;
+		private static final int NO_CONNECTION = 3;
 
-        @Override
-        protected Integer doInBackground(String... userCredentials) {
-            try {
-                connection.login(userCredentials[0], userCredentials[1]);
-            } catch (InvalidDataException e) {
-                return INVALID_CREDENTIALS;
-            } catch (ConnectionException e) {
-                return NO_CONNECTION;
-            }
-            return LOGIN_SUCCESS;
-        }
+		@Inject
+		public LoginTask(Context context) {
+			this.context = context;
+		}
 
-        @Override
-        protected void onPostExecute(Integer result) {
-            super.onPostExecute(result);
-            progressDialog.dismiss();
-            switch (result) {
-                case LOGIN_SUCCESS:
-                    Intent intent = new Intent(context, MainActivity.class);
-                    startActivity(intent);
-                    break;
+		@Override
+		protected void onPreExecute() {
+			progressDialog.show();
+		}
 
-                case INVALID_CREDENTIALS:
-                    Toast.makeText(context, context.getString(R.string.invalidCredentials), Toast.LENGTH_LONG).show();
-                    break;
+		@Override
+		protected Integer doInBackground(String... userCredentials) {
+			try {
+				connection.login(userCredentials[0], userCredentials[1]);
+			} catch (InvalidDataException e) {
+				return INVALID_CREDENTIALS;
+			} catch (ConnectionException e) {
+				return NO_CONNECTION;
+			}
+			return LOGIN_SUCCESS;
+		}
 
-                case NO_CONNECTION:
-                    Toast.makeText(context, context.getString(R.string.noConnection), Toast.LENGTH_LONG).show();
-                    break;
-            }
-        }
-    }
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+			progressDialog.dismiss();
+			switch (result) {
+			case LOGIN_SUCCESS:
+				Intent intent = new Intent(context, MainActivity.class);
+				startActivity(intent);
+				break;
+
+			case INVALID_CREDENTIALS:
+				Toast.makeText(context, context.getString(R.string.invalidCredentials), Toast.LENGTH_LONG).show();
+				break;
+
+			case NO_CONNECTION:
+				Toast.makeText(context, context.getString(R.string.noConnection), Toast.LENGTH_LONG).show();
+				break;
+			}
+		}
+	}
 }
